42bdbae2c9275bdaeb8ad671df88f22cefb09671
uzairmirza <53081780+uzairmirza90@users.noreply.github.com>
2022-06-21 20:48:00 +0500

    added server

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,17 @@
 const express = require('express')
-const app = express()
+const dotenv = require('dotenv')
+const connectDB = require('./config/db')
+const userRoutes = require('./routes/userRoutes')
 
-app.listen(6000, console.log('Server is running'))
+const app = express()
+dotenv.config()
+connectDB()
+
+app.get('/', (req, res, next) => {
+    res.send('Api is running')
+})
+
+app.use('/api/users', userRoutes)
+
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, console.log('Server is running'));
